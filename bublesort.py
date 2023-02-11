c26426f2816b012db10d3c09b60d3c0117ebce08
okSYS <ViacheslavDan803@gmail.com>
2023-02-11 02:04:25 +0200

    buble sort done

--- bublesort.py
+++ bublesort.py
@@ -1,3 +1,21 @@
-nums = [x for x in range(5000)]
 
-print(len(nums))
+from time import perf_counter
+import random
+
+def bublesort(nums):
+    n = len(nums)
+ 
+    for i in range(n):
+        for j in range(0, n-i-1):
+            if nums[j] > nums[j+1]:
+                nums[j], nums[j+1] = nums[j+1], nums[j]
+
+    return nums
+ 
+
+nums = random.sample(range(0, 1000), 1000)
+
+start = perf_counter()
+print(bublesort(nums))
+print(f"time: {(perf_counter() - start):.02f}")
+
